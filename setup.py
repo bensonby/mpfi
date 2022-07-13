6e5738d8d877157a780602c0477ff2684c73cc28
bensonby <bensonby@users.noreply.github.com>
2022-07-13 20:31:04 +0800

    Bump version to 0.0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mpfi",
-    version="0.0.12",
+    version="0.0.13",
     author="Benson Yeung",
     author_email="bensonby@gmail.com",
     description="FIS Prophet Model Point File Inspector and Processor",
